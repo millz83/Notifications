72836389305f5e5bae84ed325afd3b58627269e3
Mildred <mbrooklyn83@gmail.com>
2021-04-10 23:03:49 -0400

    script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+const button = document.getElementById('button')
+const toasts = document.getElementById('toasts')
+
+const messages = [
+    'You got mail',
+    'New Text Message',
+    'New Reply',
+    'Hello',
+]
+
+const types = ['info', 'success', 'error']
+
+button.addEventListener('click' , ()  => createNotification())
+
+function createNotification(message = null, type = null) {
+    const notif = document.createElement('div')
+    notif.classList.add('toast')
+    notif.classList.add(type ? type : getRandomtype())
+
+    notif.innerText =  message ? message : getRandomMessage()
+
+    toasts.appendChild(notif)
+
+    setTimeout(() => {
+        notif.remove()
+
+    
+    }, 3000)
+}
+
+function getRandomMessage() {
+    return messages[Math.floor(Math.random() * messages.length)]
+}
+
+function getRandomtype() {
+    return types[Math.floor(Math.random() * types.length)]
+}
